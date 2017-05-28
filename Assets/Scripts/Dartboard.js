fe1871da73a5ca894694d457296b19b16f10c883
Omar Einea <eineao@gmail.com>
2017-05-28 10:15:51 +0400

    Add Darts Stack, Destroy Darts & Final Touches

--- Assets/Scripts/Dartboard.js
+++ Assets/Scripts/Dartboard.js
@@ -1,15 +1,6 @@
 ﻿#pragma strict
 
-function Start () {
-	
-}
-
-function Update () {
-	
-}
-
 function OnTriggerEnter(col:Collider) {
-	print("Hit Dartboard");
 	col.transform.GetComponent.<Rigidbody>().velocity = Vector3.zero;
 	col.transform.GetComponent.<MoveDart>().enabled = false;
 	col.transform.SetParent(transform.parent.parent);
--- Assets/Scripts/MoveDart.js
+++ Assets/Scripts/MoveDart.js
@@ -4,9 +4,15 @@ private var rb:Rigidbody;
 
 function Start () {
 	rb = GetComponent.<Rigidbody>();
-	rb.AddForce(Quaternion.Euler(Random.Range(-20, 20), Random.Range(-20, 20), 0) * transform.forward * 10);
+	rb.AddForce(Quaternion.Euler(Random.Range(0, 15), 0, 0) * transform.forward * 200);
 }
 
 function Update () {
 	transform.forward = rb.velocity;
+	DestroyIfNotEnabled();
+}
+
+function DestroyIfNotEnabled() {
+	yield WaitForSeconds(5);
+	if(enabled) Destroy(gameObject);
 }
